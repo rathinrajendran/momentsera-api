80d62257c5c2c030d297810b5c64e31ef360942e
rathinrajendran <50700754+rathinrajendran@users.noreply.github.com>
2026-07-10 19:26:25 +0530

    env changes updated

--- src/auth/email.ts
+++ src/auth/email.ts
@@ -1,12 +1,14 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
+
   host: process.env.SMTP_HOST || "smtp.mailtrap.io",
   port: parseInt(process.env.SMTP_PORT || "2525"),
   auth: {
     user: process.env.SMTP_USER || "",
     pass: process.env.SMTP_PASS || "",
   },
+
 });
 
 export async function sendMail({
